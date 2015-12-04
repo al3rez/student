8a5444ece9f2a1581a2a356552ad134768ccb149
Alireza Bashiri <azbshiri@gmail.com>
2015-12-04 20:51:44 +0330

    Change all the messages
    * Achieves a better understanding of their meaning

--- src/trimester.c
+++ src/trimester.c
@@ -8,9 +8,9 @@ void read_student() {
     char first_name[255],
          last_name[255];
 
-    printf("First Name: ");
+    printf("Enter your first name: ");
     fgets(first_name, sizeof(first_name), stdin);
-    printf("Last Name: ");
+    printf("Enter your last name: ");
     fgets(last_name, sizeof(last_name), stdin);
 }
 int check_for_discount(float average) {
@@ -37,11 +37,11 @@ bool check_to_suspend(float average) {
 void show_message(bool suspend, int discount, float average)
 {
     if (suspend != true && discount != 0)
-        printf("The Student's Average Marks Is %0.1f And [He/She] Gets A Discount Of %d\n", average, discount);
+        printf("Your average marks is %0.1f and you get a discount of %d\n", average, discount);
 
     if (suspend != true && discount == 0)
-        printf("The Student's Average Marks Is %0.1f But As A Student [He/She] Does Not Unfortunately, Get A Discount\n", average, discount);
+        printf("Your average marks is %0.1f but you don't unfortunately, get a discount\n", average, discount);
 
     if (suspend == true && discount == 0)
-        printf("The Student's Average Marks Is %0.1f So [He/She] Gets Suspended For One Semester\n", average, discount);
+        printf("Your average marks is %0.1f so you get suspended for one trimester\n", average, discount);
 }
