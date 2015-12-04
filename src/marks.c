6dad07a76fce27591b93f1a9d39b86abe19efb0c
Alireza Bashiri <azbshiri@gmail.com>
2015-12-04 20:02:23 +0330

    Use the size of the array passed in
    * It'll work for any size array

--- src/marks.c
+++ src/marks.c
@@ -21,7 +21,7 @@ float calculate_average(float marks[], int size)
 {
     float sum = 0;
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < size; i++)
         sum += marks[i];
 
     return sum / size;
